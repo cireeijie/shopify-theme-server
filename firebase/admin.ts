73197b3f0f861fc776a1f4029f1e2e22c597378d
Eric <ericjohnariate@gmail.com>
2025-09-25 13:04:35 +0800

    initial commit

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,16 +1,14 @@
 // firebase/admin.ts
 import admin from "firebase-admin";
 
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT as string
+);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
-export const firestore = admin.firestore();
 export default admin;
