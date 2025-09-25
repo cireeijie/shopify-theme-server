a8284d267726c4a0ec9b7ecf54e84b6038755176
Eric <ericjohnariate@gmail.com>
2025-09-25 17:57:55 +0800

    update cookie login

--- api/user/login.ts
+++ api/user/login.ts
@@ -38,7 +38,7 @@ async function handler(req: VercelRequest, res: VercelResponse) {
       serialize("session", sessionCookie, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: expiresIn / 1000,
         path: "/",
       })
